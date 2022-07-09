cdbf65f8352341489871c0d668d5188c6734e42d
xavier1of1 <xkubancik@gmail.com>
2022-07-09 16:27:17 -0400

    Co-authored-by: NamanANamanA <NamanANamanA@users.noreply.github.com>

---
+++ MainScraper.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+
+
+
+
---
+++ ScrapeCL.py
@@ -0,0 +1,4 @@
+import requests
+from bs4 import BeautifulSoup
+#import numpy as np
+#import pandas as pd
