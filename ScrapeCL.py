a7a0d1db2fdd95733e2d0e035951a318e85c51a0
xavier1of1 <xkubancik@gmail.com>
2022-07-18 17:06:10 -0400

    Amazon scraper

--- ScrapeCL.py
+++ ScrapeCL.py
@@ -2,3 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 #import numpy as np
 #import pandas as pd
+
+#open facebook marketplace page
+url = 'https://www.facebook.com/marketplace/'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
